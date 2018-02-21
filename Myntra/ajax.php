38d04c932b7c473a3776010a1df6323ee668469b
Ahmed Arsalan <arsalaanmuallim28@gmail.com>
2018-02-21 18:39:19 +0530

    Updated

--- Myntra/ajax.php
+++ Myntra/ajax.php
@@ -3,10 +3,13 @@ include"conf.php";
 $colour="";
 $brand="";
 $size="";
+$price="";
 
 $colour=isset($_REQUEST['color'])?$_REQUEST['color']:"";
 $brand=isset($_REQUEST['brand'])?$_REQUEST['brand']:"";
 $size=isset($_REQUEST['size'])?$_REQUEST['size']:"";
+$price=isset($_REQUEST['price'])?$_REQUEST['price']:"";
+// print_r($price);
 
 	$query="select * from products where product_status='1' ";
 
@@ -29,13 +32,32 @@ $size=isset($_REQUEST['size'])?$_REQUEST['size']:"";
 		
 	}
 
+	if(!empty($price)){
+		$k=$price[0];
+		// echo $k;
+		// return;
+		$pr=explode("-", $k);
+		
+		$min=$pr[0];
+		// echo "Min:".$min;
+		
+		$sz=sizeof($price);
+		$k=$price[$sz-1];
+		$max=explode("-", $k);
+		// echo "Max :".$max[1];
+		$m=$max[1];
+		// return;
+		$query .= " and price between $min and $m ORDER BY price";
+		
+	}
+
 	$rs=mysql_query($query,$conn) or die("Error :" .mysql_error());
 
 	while($pro_data=mysql_fetch_assoc($rs)) {
 
 					?>
 
-					<div class="col-sm-4 col-lg-4 col-md-4">
+					<div class="col-sm-3 col-lg-3 col-md-3">
 						<div class="thumbnail"  >
 							<img src="images/products/<?php echo $pro_data['image'];?>" alt="" width="100px" height="240px">
 							<div class="caption">
@@ -47,4 +69,4 @@ $size=isset($_REQUEST['size'])?$_REQUEST['size']:"";
 						</div>
 					</div>
 				<?php } ?>
-
+	
--- Myntra/index.php
+++ Myntra/index.php
@@ -1,11 +1,13 @@
 <?php
 include"conf.php"; 
+
 ?>
 
 
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title></title>
 	 <!-- Bootstrap Core CSS -->
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -13,23 +15,76 @@ include"conf.php";
   	<link href="css/style.css" rel="stylesheet">
  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   		 <script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+  		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<style >
+  		.list-group-item{
+  			padding: 2px;
+  		}
+  		h3{
+  			margin-top: 3px;
+  			font-size: 15px;
+  		}
+  		div.list-group{
+  			margin-bottom: 3px;
+  		}
+  		input[type=checkbox]{
+  			width:20px;
+  			height: 15px;
+
+  		}
+  		a.list-group-item{
+  			border-bottom:1px solid #DC143C;
+  		}
+  		a.list-group-item:hover{
+  			color:#DC143C;
+  			border-left: 2px solid #DC143C;
+  			
+  		}
+  	</style>
 </head>
 <body>
 
-	<div class="container" style="padding-top:2%;" >
+	<div class="container-fluid" >
+		<div class="row" style="padding-bottom: 1%;">
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid" style="border-bottom:2px solid #DC143C;">
+			    <div class="navbar-header">
+			      <a class="navbar-brand" href="#" style="color: #DC143C">ABC STORE</a>
+			    </div>
+			    <ul class="nav navbar-nav">
+			      <li class="active"><a href="#">Home</a></li>
+			      <li><a href="#">MEN</a></li>
+			      <li><a href="#">WOMEN</a></li>
+			      <li><a href="#">KIDS</a></li>
+			      <li><a href="#">HOME & LIVING</a></li>
+			    </ul>
+			   </div>
+			</nav>
+			
+		</div>
 
         <div class="row">
             <div class="col-sm-3">
                 <p class="lead">Product filter</p>
 				
-			<!-- 	<div class="list-group">
+			<div class="list-group">
 				<h3>Price</h3>
-				<input type="hidden" class="price1" value="0" >
-                <input type="hidden" class="price2" value="3000"  >
-				<p id="priceshow"></p>
-                <div id="slider-range"></div>
-				
-                </div> -->
+				<a href="javascript:void(0);" class="list-group-item"> 
+					<input type="checkbox" class="item_filter price" value="199-499" >&nbsp;&nbsp; &#8377;199 To &#8377;499 
+				</a>
+               <a href="javascript:void(0);" class="list-group-item"> 
+               		<input type="checkbox" class="item_filter price" value="500-799">&nbsp;&nbsp; &#8377;500 To &#8377;799 
+               </a>
+               <a href="javascript:void(0);" class="list-group-item"> 
+               		<input type="checkbox" class="item_filter price" value="800-1099" >&nbsp;&nbsp; &#8377;800 To &#8377;1099 
+               </a>
+               <a href="javascript:void(0);" class="list-group-item"> 
+               		 <input type="checkbox" class="item_filter price" value="1100-1399">&nbsp;&nbsp; &#8377;1100 To &#8377;1399 
+               	</a>
+               <a href="javascript:void(0);" class="list-group-item"> 
+               		 <input type="checkbox" class="item_filter price" value="1400-1999">&nbsp;&nbsp; &#8377;1400 To &#8377;1999 
+               	</a>
+            </div> 
                 <!-- Color -->
                 <div class="list-group">
 				<h3>Colour</h3>
@@ -84,12 +139,15 @@ include"conf.php";
 			<div class="col-sm-9">
 				<div class="row product-data">
 					<?php
-						$query="select * from products where product_status='1'";
+						$page = (int) (!isset($_GET["page"]) ? 1 : $_GET["page"]);
+						$limit = 8;
+						$startpoint = ($page * $limit) - $limit;
+						$query="select * from products  LIMIT {$startpoint}, {$limit}  ";
 						$rs= mysql_query($query) or die("Error :".mysql_error());
 
 				while($pro_data=mysql_fetch_assoc($rs)) {
 					?>
-					<div class="col-sm-4 col-sm-4 col-sm-4">
+					<div class="col-sm-3 col-sm-3 col-sm-3">
 						<div class="thumbnail"  >
 							<img src="images/products/<?php echo $pro_data['image'];?>" alt="" width="100px" height="240px">
 							<div class="caption">
@@ -101,7 +159,20 @@ include"conf.php";
 						</div>
 					</div>
 					<?php } ?>
+		 
 				</div>	
+							 <?php  
+			$sql = "SELECT COUNT(pro_ID) FROM products";  
+			$rs_result = mysql_query($sql);  
+			$row = mysql_fetch_row($rs_result);  
+			$total_records = $row[0];  
+			$total_pages = ceil($total_records / $limit);  
+			$pagLink = "<div class='pgn'>";  
+			for ($i=1; $i<=$total_pages; $i++) {  
+			             $pagLink .= "<a href='index.php?page=".$i."'>".$i."</a>";  
+			};  
+			echo $pagLink ;
+			?> 
 			</div>
 			
 		</div>
@@ -111,25 +182,23 @@ include"conf.php";
 #loaderpro{text-align:center; background: url('loader.gif') no-repeat center; height: 150px;}
 </style>
 <script >
-	var colour,brand,size;
+	var colour,brand,size,price;
 	$(function(){
-		$('.item_filter').click(function(){
+		$('.item_filter').click(function(e){
+
 			$('.product-data').html('<div id="loaderpro" style="" ></div>');
 
 			colour=multiple_values('colour');
 			brand=multiple_values('brand');
 			size=multiple_values('size');
-
+			price=multiple_values('price');
 			$.ajax({
 				url:"ajax.php",
 				type:"post",
-				data:{color:colour,brand:brand,size:size},
+				data:{color:colour,brand:brand,size:size,price:price},
 				success:function(result){
 					$('.product-data').html(result);
 				}
-
-			
-
 		});
 	});
 });
